a0dc6f6ba14af8661b022cb0a46fc75d07286b13
hamza <kalamarasi610@gmail.com>
2025-08-08 17:44:50 +0500

    Update OWNER.js

--- Priyansh/commands/OWNER.js
+++ Priyansh/commands/OWNER.js
@@ -3,7 +3,7 @@ module.exports.config = {
 	name: "owner",
     version: "1.0.1",
 	hasPermssion: 0,
-	credits: "Arun", 
+	credits: "ₕₐₘZᵢ ₜᵣᵢCₖₑᵣ", 
 	description: "no prefix",
 	commandCategory: "No command marks needed",
 	usages: "...",
@@ -17,7 +17,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
      react.includes("virat") || 
 react.includes("owner")) {
 		var msg = {
-				body: "★𝗢𝘄𝗻𝗲𝗿 + ❤️...𝐕𝐢𝐫𝐚𝐭 𝐬𝐚𝐢𝐧𝐢...❤️ ● ──────────────── ●𝐕𝐢𝐫𝐚𝐭 𝐬𝐚𝐢𝐧𝐢 ● ──────────────── ●𝐎𝐰𝐧𝐞𝐫 ➻❤️....𝐕𝐢𝐫𝐚𝐭 𝐬𝐚𝐢𝐧𝐢....❤️",attachment: fs.createReadStream(__dirname + `/noprefix/owner.jpeg`)
+				body: "★𝗢𝘄𝗻𝗲𝗿 + ❤️...ₕₐₘZᵢ ₜᵣᵢCₖₑᵣ...❤️ ● ──────────────── ●ₕₐₘZᵢ ₜᵣᵢCₖₑᵣ ● ──────────────── ●𝐎𝐰𝐧𝐞𝐫 ➻❤️....ₕₐₘZᵢ ₜᵣᵢCₖₑᵣ....❤️",attachment: fs.createReadStream(__dirname + `/noprefix/owner.jpeg`)
 			}
 			api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("📷", event.messageID, (err) => {}, true)
